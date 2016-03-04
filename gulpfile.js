5056b791be0e3a4aba56fdbccabc8b0ea7e7040f
eduardo <elassolGhotmail.com>
2016-03-04 16:22:44 +0000

    add notify for error function

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@ var gulp         = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var sass         = require ('gulp-sass');
 var plumber      = require('gulp-plumber');
- 
+var notify       = require('gulp-notify'); 
 
 
+
+
+
+function customPlumber () { return plumber({
+    return plumber({
+        errorHandler: notify.onError("Error: <%= error.message %>")
+    });
+}
 
 gulp.task('sass', function(){
     return  gulp.src('theme/sass/**/*.scss')
+        .pipe(customPlumber())
         .pipe(sass({
             precision: 2
         }))
@@ -19,5 +28,7 @@ gulp.task('sass', function(){
 
  
 gulp.task('watch', function(){ 
-    gulp.watch('theme/scss/**/*.scss', ['sass']); // 
+    gulp.watch('theme/sass/**/*.scss', ['sass']); // 
 })
+
+
