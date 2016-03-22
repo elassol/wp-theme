cbb2f8b5ec3334d8cb18c9ca005f1d59e4761aa2
eduardo <elassolGhotmail.com>
2016-03-22 12:04:34 +0000

    bower and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var sass         = require ('gulp-sass');
 var plumber      = require('gulp-plumber');
 var notify       = require('gulp-notify'); 
 var browserSync  = require('browser-sync'); 
+var sourcemaps   = require('gulp-sourcemaps');
 
 
 
@@ -22,27 +23,36 @@ function customPlumber(errTitle) { return plumber({
 gulp.task('sass', function(){
     return  gulp.src('theme/sass/**/*.scss')
         .pipe(customPlumber('Error Running Sass'))
+        // inititalizr sourcemap before anyother pluging that alter  files
+        .pipe(sourcemaps.init())
         .pipe(sass({
             precision: 2
         }))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('theme/css'));
-        .pipe(browserSync.reload{
-            stream: true
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('theme/css'))
+        .pipe(browserSync.reload({
+          stream: true
         }))
-});
-
-
-
- 
-gulp.task('watch', function(){ 
-    gulp.watch('theme/sass/**/*.scss', ['sass']); // 
+        
 })
 
 
 gulp.task('browserSync', function() {
-      browserSync({
-        server: {
-          baseDir: 'app'
-    }, })
+  browserSync({
+    server: {
+      baseDir: 'theme'
+    },
+
+    // Use Wordpress.dev instead of spinning up a server
+    // proxy: wordpress.dev
+  })
 })
+
+
+ 
+gulp.task('watch', ['browserSync', 'sass'], function(){ 
+    gulp.watch('theme/sass/**/*.scss', ['sass']); // 
+})
+
+
---
+++ theme/js/main.js
@@ -0,0 +1,4 @@
+// main.js
+$(document).ready(function() {
+ console.log('main load');
+});
