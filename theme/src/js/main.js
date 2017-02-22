03f8133b8aac525a6acf9c406f7f614daf188201
eduardo <elassolGhotmail.com>
2017-02-22 15:17:52 +0000

    7 to 1 pattern

---
+++ theme/src/js/main.js
@@ -0,0 +1,29 @@
+// main.js
+$(document).ready(function() {
+    'use strict';
+    console.log('Load main JS for lassodesign');
+    // document.body.style.backgroundColor = "yellow";
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1) {
+          $('#masthead').addClass("sticky");
+          $('section.site-banner').addClass('sticky');
+        } else {
+          $('#masthead').removeClass("sticky");
+          $('section.site-banner').removeClass('sticky');
+        }
+      });
+
+    console.log('it works');
+
+  });
+
+
+// main.js
+$(document).ready(function() { // Fixing 'use strict'
+    'use strict';
+    // comparing with == generates a warning
+    if ('testing' === 'testing') {
+      console.log($);
+    }
+  });
